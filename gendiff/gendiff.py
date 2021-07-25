370d66e8b375dc83548fcee18a430c33a7b1ca2a
Kirill Chertkov <chertkov.k@gmail.com>
2021-07-25 21:21:27 +0300

    step 6. add tests for nested json and separate diff from view

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,15 +1,16 @@
 from gendiff.parser import parse
 
 
-def generate_diff(path1, path2):
+def generate_diff(path1, path2, format='stylish'):
     """Generate diff of two files
 
     Args:
-        file1 (str): Path to the first file
-        file2 (str): Path to the second file
+        path1 (str): Path to the first file
+        path2 (str): Path to the second file
+        format (str): Output format
 
     Returns:
-        string: Difference between two files
+        string: Difference between two files in selected format
     """
 
     file1 = parse(path1)
@@ -18,19 +19,39 @@ def generate_diff(path1, path2):
     keys1 = set(file1.keys())
     keys2 = set(file2.keys())
 
-    result = []
+    diff = []
 
     for key in keys1 | keys2:
         if key in keys1 and key in keys2:
             if file1[key] == file2[key]:
-                result.append(f"    {key}: {file1[key]}")
+                diff.append({
+                    "key": key,
+                    "value1": file1[key],
+                    "value2": file2[key],
+                    "status": "unchanged"
+                })
             else:
-                result.append(f"  - {key}: {file1[key]}")
-                result.append(f"  + {key}: {file2[key]}")
-        elif key in keys1:
-            result.append(f"  - {key}: {file1[key]}")
-        elif key in keys2:
-            result.append(f"  + {key}: {file2[key]}")
+                diff.append({
+                    "key": key,
+                    "value1": file1[key],
+                    "value2": file2[key],
+                    "status": "updated",
+                })
 
-    result.sort(key=lambda line: (line.split(':')[0][4:]))
-    return "{{\n{}\n}}".format('\n'.join(result))
+        elif key in keys1:
+            diff.append({
+                "key": key,
+                "value1": file1[key],
+                "value2": None,
+                "status": "removed",
+            })
+
+        elif key in keys2:
+            diff.append({
+                "key": key,
+                "value1": None,
+                "value2": file2[key],
+                "status": "added",
+            })
+
+    return diff
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -15,3 +15,11 @@ def test_yaml():
             'tests/fixtures/file1.yml',
             'tests/fixtures/file2.yml',
         ) == expected.read()
+
+
+def test_nested_json():
+    with open('tests/fixtures/nested_result.txt', 'r') as expected:
+        assert generate_diff(
+            'tests/fixtures/nested_file1.json',
+            'tests/fixtures/nested_file2.json',
+        ) == expected.read()
